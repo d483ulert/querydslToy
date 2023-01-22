4208bd9fd6c6eade0befdd57856ba7a70146fa11
d483ulert <tottenham3@naver.com>
2023-01-22 21:56:28 +0900

    controller 추가

---
+++ src/main/java/com/dev/toy/controller/BoardController.java
@@ -0,0 +1,10 @@
+package com.dev.toy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+
+}
---
+++ src/main/java/com/dev/toy/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.dev.toy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+
+
+}
