2bb5badc1f4bfad8bccafedff00c4a3b9c0f67d1
jisung7153 <tottenham3@naver.com>
2023-02-23 21:52:24 +0900

    ResultDto 추가

--- src/main/java/com/dev/toy/common/ResultListDto.java
+++ src/main/java/com/dev/toy/common/ResultListDto.java
@@ -4,31 +4,27 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class ResultDto<T>{
+public class ResultListDto<T>{
 
     private long result_code;
+
     private String result_msg;
-    private T result_data;
+    private List<T> result_data;
 
 
-    public ResultDto(long result_code, String result_msg){
+    public ResultListDto(long result_code, String result_msg){
         setResult_code(result_code);
         setResult_msg(result_msg);
     }
 
-    public ResultDto(long result_code, T result_data){
-        setResult_code(result_code);
-        setResult_data(result_data);
-    }
 
-    public ResultDto(long result_code, String result_msg, T result_data){
+    public ResultListDto(long result_code, String result_msg, List<T> result_data){
         setResult_code(result_code);
         setResult_msg(result_msg);
-        setResult_data(result_data);
     }
+
 
     //Todo
     public void setErrorNone(){}
     public void setErrorCode(){}
-
 }
